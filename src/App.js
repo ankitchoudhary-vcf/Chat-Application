ab14c77e627d72033bf0e7d7e2a403b1d93c3fee
ankit <ac2000choudhary@gmail.com>
2021-08-06 11:14:15 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ChatEngine} from 'react-chat-engine';
 import  './App.css';
+import ChatFeed from './Component/ChatFeed'
 
 export const App = () => {
     return (
@@ -10,6 +11,7 @@ export const App = () => {
                 projectID="4fcca4fa-bb1b-4bee-a528-a61f5c13a6c4"
                 userName="himanshu"
                 userSecret="123456@himanshu"
+                renderChatFeed={(chatAppProps) => < ChatFeed { ... chatAppProps } />}
             />
             
         </>
