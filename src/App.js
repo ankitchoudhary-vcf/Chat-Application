40745c9c7fc67bb3c2db33af36b7a5acf1db3d7e
ankit <ac2000choudhary@gmail.com>
2021-08-06 12:49:47 +0530

    App updated

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 import {ChatEngine} from 'react-chat-engine';
 import  './App.css';
 import ChatFeed from './Component/ChatFeed'
+import LoginForm from './Component/LoginForm'
 
 export const App = () => {
+
+    if(!localStorage.getItem('username')) {
+        return (
+            <>
+                <LoginForm/>
+            </>
+        )
+    }
     return (
         <>
             <ChatEngine
                 height="100vh"
                 projectID="4fcca4fa-bb1b-4bee-a528-a61f5c13a6c4"
-                userName="himanshu"
-                userSecret="123456@himanshu"
+                userName={ localStorage.getItem('username') }
+                userSecret={ localStorage.getItem('password') }
                 renderChatFeed={(chatAppProps) => < ChatFeed { ... chatAppProps } />}
             />
             
