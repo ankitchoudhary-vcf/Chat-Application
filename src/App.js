fabf934e810bd0631b8595b6499c1c6920c72c95
ankit <ac2000choudhary@gmail.com>
2021-08-06 10:15:35 +0530

    App.js added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {ChatEngine} from 'react-chat-engine';
+import  './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <>
+            <ChatEngine
+                height="100vh"
+                projectID="4fcca4fa-bb1b-4bee-a528-a61f5c13a6c4"
+                userName="himanshu"
+                userSecret="123456@himanshu"
+            />
+            
+        </>
+    )
 }
 
 export default App;
