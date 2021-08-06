34e21eb64b3e563b5ced71cd71b3d26a943cbbbc
ankit <ac2000choudhary@gmail.com>
2021-08-06 11:58:52 +0530

    Message Form Created

--- src/Component/MessageForm.js
+++ src/Component/MessageForm.js
@@ -1,10 +1,65 @@
 import React from 'react'
+import { useState } from 'react'
+import {sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
-export const MessageForm = () => {
+export const MessageForm = (props) => {
+
+    const [value, setValue] = useState('');
+    const { chatId, creds } = props;
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+        isTyping(props, chatId)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const text = value.trim();
+
+        if(text.length > 0){
+            sendMessage(creds, chatId, { text });   
+        }
+        else{
+            alert("Message can not to blank");
+        }
+
+        setValue('')
+
+    }
+
+    const handleUpload = (e) => {
+        e.preventDefault();
+
+        sendMessage(creds, chatId, { files: e.target.files, text: '' });
+
+    }
     return (
-        <div>
-            MessageForm
-        </div>
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input
+                className="message-input"
+                placeholder="Send a message ... "
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+            <label htmlFor="upload-button">
+                <span className="image-button">
+                    <PictureOutlined className="picture-icon" />
+                </span>
+            </label>
+            <input
+                type="file"
+                multiple={false}
+                id="upload-button"
+                style={{ display : 'none' }}
+                onChange={handleUpload}
+            />
+            <button type="submit" className="send-button">
+                <SendOutlined className="send-icon" />
+            </button>
+        </form>
     )
 }
 
